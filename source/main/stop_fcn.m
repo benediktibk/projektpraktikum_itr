af5a9e95bf11aa89bea7faf5631bd19063bb7a9d
Wirnshofer Florian <florian.wirnshofer@hotmail.de>
2014-10-22 14:18:28 +0200

    Automatic Close Script

---
+++ source/main/stop_fcn.m
@@ -0,0 +1,24 @@
+
+
+if(exist(['~/ppData/',date],'dir') == 0)
+    mkdir(['~/ppData/',date]);
+end
+
+i= 1;
+while(exist(['~/ppData/',date,'/Measurement_',num2str(i)],'dir') ~= 0)
+    i = i+1;
+end
+mkdir(['~/ppData/',date,'/Measurement_',num2str(i)]);   
+!scp robot1Platform:pp_ident/workspace/*.dat ./
+%copyfile('Data_Analysis.m',['~/ppData/',date,'/Measurement_',num2str(i)])
+copyfile('*.dat',['~/ppData/',date,'/Measurement_',num2str(i)])
+%copyfile('Kuka_Pendel_INIT.m',['~/ppData/',date,'/Measurement_',num2str(i)])
+copyfile('control.slx',['~/ppData/',date,'/Measurement_',num2str(i)])
+
+delete('*.dat')
+delete('*.mat')
+
+%cd(['Data/',date,'/Measurement_',num2str(i)])
+clc
+close all
+
