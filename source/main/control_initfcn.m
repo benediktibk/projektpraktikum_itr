8c1fe54f6e5d13d0bb2c2cb7a0474b50a284e2d5
Wirnshofer Florian <florian.wirnshofer@hotmail.de>
2014-10-24 11:11:24 +0200

    Added Velocity Control

--- source/main/control_initfcn.m
+++ source/main/control_initfcn.m
@@ -1,5 +1,20 @@
+%% Includes
+addpath('rot2Q/')
+
 %% Sampling Time Ts 
 Ts = 0.001;
 
 %% Initial Poses
-initPoseRightArm = [6 .7 -.23 .9 1 0 0 0 0 10 zeros(1,13)];
+initPoseRightArm = [6 .7 -.23 .9 1 0 0 0  0 10 zeros(1,13)];
+
+%% Scary 
+id = 102;
+dx = 0;
+dy = 0.005;
+dz = 0;
+ox = 0;
+oy = 0;
+oz = 0;
+elbow = 0;
+
+testVelocity = [id dx dy dz ox oy oz elbow zeros(1,15)];
