eea2dc1c04199cbaac91a83db993866e6c0e5939
Wirnshofer Florian <florian.wirnshofer@hotmail.de>
2014-10-24 11:17:58 +0200

    small changes in init

--- source/main/control_initfcn.m
+++ source/main/control_initfcn.m
@@ -7,8 +7,10 @@ Ts = 0.001;
 %% Initial Poses
 initPoseRightArm = [6 .7 -.23 .9 1 0 0 0  0 10 zeros(1,13)];
 
-%% Scary 
-id = 102;
+%% Velocity Stream ID
+% Can be found in one of the headders of the sfunction
+MANIPULATION_CMD_CONFIGURATION_VELOCITY = 102;
+
 dx = 0;
 dy = 0.005;
 dz = 0;
@@ -17,4 +19,4 @@ oy = 0;
 oz = 0;
 elbow = 0;
 
-testVelocity = [id dx dy dz ox oy oz elbow zeros(1,15)];
+testVelocity = [MANIPULATION_CMD_CONFIGURATION_VELOCITY dx dy dz ox oy oz elbow zeros(1,15)];
