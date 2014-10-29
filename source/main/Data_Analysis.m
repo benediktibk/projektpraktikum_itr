b85226b62bf32a47e1a92e085f4cb0d1b64ef74f
Wirnshofer Florian <florian.wirnshofer@hotmail.de>
2014-10-29 10:19:20 +0100

    added activity logger to DataAnalysis

--- source/main/Data_Analysis.m
+++ source/main/Data_Analysis.m
@@ -1,6 +1,6 @@
 clc
 close all
-clearvars -EXCEPT Ts initPoseRightArm
+clearvars -EXCEPT Ts initPoseRightArm dataLoggerStartTime dataLoggerStopTime
 
 
 
@@ -9,3 +9,9 @@ files = dir('*.dat');
 for i=1:length(files)
     eval(['load ' files(i).name ' -ascii']);
 end
+
+start = find(active,1);
+
+
+%L = length(theta_star);
+%t= 0:0.001:(L-1)*0.001;
