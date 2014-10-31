abef664320e93c5d568672caa6b470a9dc319501
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2014-10-31 16:10:36 +0100

    visualize p and q

--- source/estimation/estimation_stopfcn.m
+++ source/estimation/estimation_stopfcn.m
@@ -1,1 +1,9 @@
-%% Visualization 
+%% Visualization
+close all
+clearvars -EXCEPT p theta_hat c
+clc
+plot3(p(:,1),p(:,2),p(:,3))
+hold on
+q = p +repmat(c',size(p,1),1);
+plot3(q(:,1),q(:,2),q(:,3),'r')
+
