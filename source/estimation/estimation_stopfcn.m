d29fbd170b73ead501de0a50db712f7d742ff146
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2014-10-31 16:15:33 +0100

    added a line

--- source/estimation/estimation_stopfcn.m
+++ source/estimation/estimation_stopfcn.m
@@ -6,4 +6,6 @@ plot3(p(:,1),p(:,2),p(:,3))
 hold on
 q = p +repmat(c',size(p,1),1);
 plot3(q(:,1),q(:,2),q(:,3),'r')
-
+for i = 1:100:size(p,1)
+    line([p(i,1),q(i,1)],[p(i,2),q(i,2)],[p(i,3),q(i,3)]);
+end
