63369451ebe036c03f4066116107b61e81d40e20
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2014-10-31 15:53:02 +0100

    added stop fcn and Inertia to init

--- source/estimation/estimation_initfcn.m
+++ source/estimation/estimation_initfcn.m
@@ -2,3 +2,14 @@ addpath('functions/');
 
 %% Sampling Time Ts 
 Ts = 0.001;
+
+%% Object Properties
+
+m = 2 ;
+x = 0.5;
+y = 0.3;
+z = 0.03;
+I_xx = 1/12*m*(y^2+z^2);
+I_yy = 1/12*m*(x^2+z^2);
+I_zz = 1/12*m*(y^2+x^2);
+I_q  = diag([I_xx,I_yy,I_zz]);
---
+++ source/estimation/estimation_stopfcn.m
@@ -0,0 +1,4 @@
+addpath('functions/');
+
+%% Sampling Time Ts 
+Ts = 0.001;
