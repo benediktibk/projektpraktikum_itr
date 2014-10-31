2032e85175cef73d8ee06900e4bec7467c79fb88
Florian Wirnshofer <florian.wirnshofer@hotmail.de>
2014-10-31 16:24:12 +0100

    finished visualization of trajectory

--- source/estimation/estimation_stopfcn.m
+++ source/estimation/estimation_stopfcn.m
@@ -7,5 +7,13 @@ hold on
 q = p +repmat(c',size(p,1),1);
 plot3(q(:,1),q(:,2),q(:,3),'r')
 for i = 1:100:size(p,1)
-    line([p(i,1),q(i,1)],[p(i,2),q(i,2)],[p(i,3),q(i,3)]);
+    plot3([p(i,1),q(i,1)],[p(i,2),q(i,2)],[p(i,3),q(i,3)],'g--');
 end
+plot3([0 1],[0 0],[0 0],'k','Linewidth',3)
+plot3([0 0],[0 1],[0 0],'k','Linewidth',3)
+plot3([0 0],[0 0],[0 1],'k','Linewidth',3)
+axis equal
+grid on
+xlabel('x')
+ylabel('y')
+zlabel('z')
