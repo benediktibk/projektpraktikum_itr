21f3b39420f9be31a7349355a39a18fbe401b9ee
Benedikt Schmidt <benediktibk@aon.at>
2014-11-17 13:30:13 +0100

    cleaned up data analysis

--- source/main/Data_Analysis.m
+++ source/main/Data_Analysis.m
@@ -1,30 +1,30 @@
+%% clean up from previous runs
 clc
 close all
 clearvars -EXCEPT Ts initPoseRightArm dataLoggerStartTime dataLoggerStopTime
 
-
-
+%% load all files
 files = dir('*.dat');
 
 for i=1:length(files)
     eval(['load ' files(i).name ' -ascii']);
 end
 
-
-start = 1;
-stop = 50000;
-
-%L = length(theta_star);
-%t= 0:0.001:(L-1)*0.001;
-y = objectPosWC(start:stop,1);
-L = length(y);
-t= 0:0.001:(L-1)*0.001;
-ysm = smooth(y,0.01,'loess');
-dv= diff(ysm);
-dt =diff(t)';
-vel = dv./dt;
-plot(vel)
-hold on
-
-plot(filt2(start:stop,1),'r')
-plot(filt5(start:stop,1),'g')
+%% plot some useful graphs
+% start = 1;
+% stop = 50000;
+% 
+% L = length(theta_star);
+% t= 0:0.001:(L-1)*0.001;
+% y = objectPosWC(start:stop,1);
+% L = length(y);
+% t= 0:0.001:(L-1)*0.001;
+% ysm = smooth(y,0.01,'loess');
+% dv= diff(ysm);
+% dt =diff(t)';
+% vel = dv./dt;
+% plot(vel)
+% hold on
+% 
+% plot(filt2(start:stop,1),'r')
+% plot(filt5(start:stop,1),'g')
