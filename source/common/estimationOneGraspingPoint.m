e0f405307afea2a7dd79fb6766af5ff45065935d
Benedikt Schmidt <benediktibk@aon.at>
2014-11-18 15:46:48 +0100

    extracted least squares as seperate function

--- source/common/estimationOneGraspingPoint.m
+++ source/common/estimationOneGraspingPoint.m
@@ -1,54 +1,5 @@
-function thetaHat = estimationOneGraspingPoint(F,N,omega,domega,ddp,g,forgettingFactor,reinitialize)
-    persistent thetaHatPrevious PPrevious initializationNecessary;
-
-    if isempty(initializationNecessary)
-        initializationNecessary = 1;
-    end
-    
-    if reinitialize == 1
-        initializationNecessary = 1;
-    end
-
-    % calculate linear relation Phi between the estimates and the input
-    omegaCross = crossMatrix(omega);
-    domegaCross = crossMatrix(domega);
-    omegaDot = dotMatrix(omega);
-    domegaDot = dotMatrix(domega);
+function thetaHat = estimationOneGraspingPoint(F, N, omega, domega, ddp, g, forgettingFactor, reinitialize)
     h = [F;N];
-    Phi = [ddp+g , domegaCross+omegaCross*omegaCross, zeros(3,6) ; zeros(3,1) , crossMatrix(-g-ddp) , domegaDot+omegaCross*omegaDot];
-
-    % intialize persisent variables
-    if initializationNecessary == 1 || isempty(PPrevious)
-        PPrevious = inv(Phi'*Phi);
-        thetaHatPrevious = PPrevious*Phi'*h;
-        thetaHat = thetaHatPrevious;
-        initializationNecessary = 0;
-        return;
-    end    
-
-    % check for invalid values in the current state
-    if any(any(isnan(PPrevious)))
-        initializationNecessary = 1;
-        thetaHat = thetaHatPrevious;
-        return;  
-    end    
-
-    KInternal = eye(6)+Phi*PPrevious*Phi';
-    KInternalInv = inv(KInternal);
-
-    % check for invalid values in the current estimation step
-    if calculateConditionDistance(KInternal, KInternalInv) > 1e20
-        initializationNecessary = 1;
-        thetaHat = thetaHatPrevious;
-        return;  
-    end
-
-    % calculate improved estimation
-    K = PPrevious*Phi'*KInternalInv;
-    PNew = (eye(10)-K*Phi)*PPrevious;
-    P = (1 - forgettingFactor)*PNew + forgettingFactor*PPrevious;
-    thetaHatNew = thetaHatPrevious+K*(h-Phi*thetaHatPrevious);
-    thetaHat = (1 - forgettingFactor)*thetaHatNew + forgettingFactor*thetaHatPrevious;
-    PPrevious = P;
-    thetaHatPrevious = thetaHat;
+    inputs = [ddp; omega; domega; g];
+    thetaHat = recursiveLeastSquares(h, inputs, @estimationOneGraspingPointPhi, forgettingFactor, reinitialize);
 end
---
+++ source/common/recursiveLeastSquares.m
@@ -0,0 +1,50 @@
+function thetaHat = recursiveLeastSquares(measurements, inputs, linearRelation, forgettingFactor, reinitialize)
+    persistent thetaHatPrevious PPrevious initializationNecessary;
+
+    if isempty(initializationNecessary)
+        initializationNecessary = 1;
+    end
+    
+    if reinitialize == 1
+        initializationNecessary = 1;
+    end
+
+    % calculate linear relation Phi between the estimates and the input
+    Phi = linearRelation(inputs);    
+
+    % intialize persisent variables
+    if initializationNecessary == 1 || isempty(PPrevious)
+        PPrevious = inv(Phi'*Phi);
+        thetaHatPrevious = PPrevious*Phi'*measurements;
+        thetaHat = thetaHatPrevious;
+        initializationNecessary = 0;
+        return;
+    end    
+
+    % check for invalid values in the current state
+    if any(any(isnan(PPrevious)))
+        initializationNecessary = 1;
+        thetaHat = thetaHatPrevious;
+        return;  
+    end    
+
+    KInternal = eye(6)+Phi*PPrevious*Phi';
+    KInternalInv = inv(KInternal);
+
+    % check for invalid values in the current estimation step
+    if calculateConditionDistance(KInternal, KInternalInv) > 1e20
+        initializationNecessary = 1;
+        thetaHat = thetaHatPrevious;
+        return;  
+    end
+
+    % calculate improved estimation
+    K = PPrevious*Phi'*KInternalInv;
+    PNew = (eye(10)-K*Phi)*PPrevious;
+    P = (1 - forgettingFactor)*PNew + forgettingFactor*PPrevious;
+    thetaHatNew = thetaHatPrevious+K*(measurements-Phi*thetaHatPrevious);
+    thetaHat = (1 - forgettingFactor)*thetaHatNew + forgettingFactor*thetaHatPrevious;
+    PPrevious = P;
+    thetaHatPrevious = thetaHat;
+end
+
