e99da54de88c29aa9bf21e95317d8639884f45dd
Benedikt Schmidt <benediktibk@aon.at>
2014-11-19 10:18:50 +0100

    removed debugging facilities

--- source/common/estimationThreeGraspingPoints.m
+++ source/common/estimationThreeGraspingPoints.m
@@ -1,4 +1,4 @@
-function [thetaHat, F, N] = estimationThreeGraspingPoints(F1, F2, F3, N1, N2, N3, omega, domega, ddp, g, r12, r13, forgettingFactor, reinitialize)
+function thetaHat = estimationThreeGraspingPoints(F1, F2, F3, N1, N2, N3, omega, domega, ddp, g, r12, r13, forgettingFactor, reinitialize)
     F = F1 + F2 + F3;
     N = N1 + N2 + N3 + cross(r12, F2) + cross(r13, F3);
     h = [F;N];
