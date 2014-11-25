74b90d03d32480499a7c629292ec6f5d1ad66ff1
Wirnshofer Florian <florian.wirnshofer@hotmail.de>
2014-11-25 12:22:14 +0100

    changed initialization manip

--- scripts/initialize-manipulator.sh
+++ scripts/initialize-manipulator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/wearhap/src/actuators/manipulation/accrea/armInitialization 
-./newInitialization JointInitialization.xml
+newInitialization JointInitialization.xml
 cd ~/wearhap/src/actuators/manipulation/accrea/FTSInitialization
 ./new_goto_qa
 ./FTS_dual_Initialization
