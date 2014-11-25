b82703f49b630b4cec9501550a4ab46465cb031d
Wirnshofer Florian <florian.wirnshofer@hotmail.de>
2014-11-25 12:16:19 +0100

    kill all processed during initialization

--- scripts/initialize-platform.sh
+++ scripts/initialize-platform.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 rtdb-stop
+killall -9 kogmo_rtdb_man
+killall -9 Qualisys2Simulink
+killall -9 IceServerStarter
+killall -9 IceServerGripper
+killall -9 IceServerManipulation
+killall -9 IceServerLocomotion
+killall -9 QuattrobotControl
+killall -9 GuiStarter
+killall -9 GuiGripper
+killall -9 GripperDriver
+
 rtdb-start
 Qualisys2Simulink & IceServerStarter & GuiStarter & GuiGripper
